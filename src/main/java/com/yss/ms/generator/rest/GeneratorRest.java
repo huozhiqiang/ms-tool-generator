fdf66c0a21f05db569af3feb72ce0930969ae4ad
huozhiqiang <huo649288142@163.com>
2019-01-25 11:30:25 +0800

    添加返回Response

--- src/main/java/com/yss/ms/generator/rest/GeneratorRest.java
+++ src/main/java/com/yss/ms/generator/rest/GeneratorRest.java
@@ -1,7 +1,7 @@
 package com.yss.ms.generator.rest;
 
 import com.alibaba.fastjson.JSON;
-import com.yss.ms.common.msg.TableResultResponse;
+import com.yss.ms.generator.response.TableResultResponse;
 import com.yss.ms.generator.service.GeneratorService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
